d5c23d472f768c31e3d8ff32ff06c19568e6f544
andys <andy.smith@mkodo.com>
2019-05-05 20:23:08 +0100

    Initial Commit

---
+++ VideoStore/VideoStore.h
@@ -0,0 +1,19 @@
+//
+//  VideoStore.h
+//  VideoStore
+//
+//  Created by Andy Smith on 05/05/2019.
+//  Copyright © 2019 Andy Smith. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for VideoStore.
+FOUNDATION_EXPORT double VideoStoreVersionNumber;
+
+//! Project version string for VideoStore.
+FOUNDATION_EXPORT const unsigned char VideoStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VideoStore/PublicHeader.h>
+
+
