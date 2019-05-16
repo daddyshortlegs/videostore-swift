8ccfba408cbd9a994bb950e3f875b1c634c0a184
Tris <tristram.dev@gmail.com>
2019-05-16 15:36:13 +0100

    extracted frequent renter points

--- VideoStore/Statement.swift
+++ VideoStore/Statement.swift
@@ -6,6 +6,16 @@ public class Statement {
     self.customer = customer
   }
   
+  func calculateFrequentRenterPoints(rental: Rental) -> Int {
+    var frequentRenterPoints = 1
+    
+    if rental.movie.priceCode == Movie.NEW_RELEASE
+      && rental.daysRented > 1 {
+      frequentRenterPoints += 1
+    }
+    return frequentRenterPoints
+  }
+  
   public func generate() -> String {
     var totalAmount: Double = 0
     var frequentRenterPoints: Int = 0
@@ -15,12 +25,7 @@ public class Statement {
     for rental in customer.rentals {
       var thisAmount: Double = 0
       thisAmount += rental.movie.calculateAmount(rental)
-      frequentRenterPoints+=1
-      
-      if rental.movie.priceCode == Movie.NEW_RELEASE
-        && rental.daysRented > 1 {
-        frequentRenterPoints+=1
-      }
+      frequentRenterPoints += calculateFrequentRenterPoints(rental: rental)
       
       result += "\t\(rental.movie.title)\t\(thisAmount)\n"
       totalAmount += thisAmount
